1884aa5c2fe6911b5a5b90ff26b645145710fa39
CovenEsme <84377742+CovenEsme@users.noreply.github.com>
2022-10-01 14:27:17 +0100

    Add option to limit the max number of demises to 1

--- options.py
+++ options.py
@@ -208,6 +208,9 @@ class Options:
         if self["random-settings-weighting"] == "Random":
             with (RANDO_ROOT_PATH / "rs_weightings" / "random.yaml").open("r") as f:
                 RS_WEIGHTING = yaml.safe_load(f)
+        elif self["random-settings-weighting"] == "Random Max 1 Demise":
+            with (RANDO_ROOT_PATH / "rs_weightings" / "random_max_1_demise.yaml").open("r") as f:
+                RS_WEIGHTING = yaml.safe_load(f)
         elif self["random-settings-weighting"] == "Fast":
             with (RANDO_ROOT_PATH / "rs_weightings" / "fast.yaml").open("r") as f:
                 RS_WEIGHTING = yaml.safe_load(f)
